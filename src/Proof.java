ba7acaccb04e678d2d4d154b9506cff00c2857c7
nanikon <nata.nik.18@mail.ru>
2022-01-28 09:26:26 +0300

    add struct for proof

---
+++ src/Proof.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Natalia Nikonova
+ */
+public class Proof {
+    private Node alfa;
+    private Node betta;
+    private List<Node> context = new LinkedList<>();
+    private List<Node> proof = new LinkedList<>();
+
+    public Node getAlfa() {
+        return alfa;
+    }
+
+    public void setAlfa(Node alfa) {
+        this.alfa = alfa;
+    }
+
+    public Node getBetta() {
+        return betta;
+    }
+
+    public void setBetta(Node betta) {
+        this.betta = betta;
+    }
+
+    public List<Node> getContext() {
+        return context;
+    }
+
+    public List<Node> getProof() {
+        return proof;
+    }
+
+    public void addToContext(Node node) {
+        this.context.add(node);
+    }
+
+    public void addToProofEnd(Node node) {
+        this.proof.add(node);
+    }
+
+    public void addTpProofBefore(Node before, Node insert) {
+        this.proof.add(this.proof.indexOf(before), insert);
+    }
+}
